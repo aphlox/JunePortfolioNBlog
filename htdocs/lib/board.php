f5541e1ec135fe44667742d1dd8ccff6b4717748
june <aphlox@naver.com>
2019-11-18 18:43:18 +0900

    페이징 - 페이징 완성 예외처리까지

--- htdocs/lib/board.php
+++ htdocs/lib/board.php
@@ -1,25 +1,46 @@
 <?php
-function print_list($page){
+function print_list($page)
+{
 
     $list = scandir('./data');
-    $list_result = array_diff($list, array('.', '..'));
-    $list_result = array_values($list_result);
-    for ($count = $page; $count >= 0; $count--) {
+    $listResult = array_diff($list, array('.', '..'));
+    $listResult = array_values($listResult);
+    $lastPage = floor(count($listResult) / 10) + 1;
+    $selectStart = count($listResult) - 10 * ($page - 1);
+    $selectEnd = count($listResult) - 10 * ($page);
+    for ($count = $selectStart; $count > $selectEnd; $count--) {
 
-        echo '
+        if ($count == 0) {
+            break;
+
+        }
+        else{
+
+            echo '
                 <tr>
-                    <th scope="row" class="mobile" style="text-align:center;">' . ($count+1) . '</th>
+                
+                    <th scope="row" class="mobile" style="text-align:center;">' . ($count) . '</th>
                     
-                    <td><a href="../boardView.php?id='.$list_result[$count].'" style="color: #000000;">' . $list_result[$count] . '</a></td>
+                    <td><a href="../boardView.php?id=' . $listResult[$count - 1] . '" style="color: #000000;">' . $listResult[$count - 1] . '</a></td>
 <!--
-<td class="mobile" style="text-align:center;">'.$page.'</td>
+<td class="mobile" style="text-align:center;">' . $page . '</td>
                     <td class="mobile" style="text-align:center;">2018-01-05</td>
 -->
                 </tr>
                 ';
-
+        }
     }
 
+
+}
+function get_this_page(){
+
+    //getPage 하는데 없으면 1페이지를 반환
+    if (isSet($_GET['page'])) {
+        return $_GET['page'];
+    } else {
+        return 1;
+    }
 
 }
 
