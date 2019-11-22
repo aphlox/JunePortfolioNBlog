97767079f1fcdad8651bae4c73f0a5e7b6a4f031
june <aphlox@naver.com>
2019-11-22 12:24:03 +0900

    로그인 구현완료+세션에 저장

--- htdocs/login.php
+++ htdocs/login.php
@@ -3,19 +3,18 @@ header("Content-Type: text/html; charset=UTF-8");
 session_start();
 $conn = new mysqli("192.168.204.138", "june", "Midarlk3134!", "juneblog");
 mysqli_query($conn,'SET NAMES utf8');
-//$id = $_POST['id'];
-//$pws = $_POST['password'];
-$id = 'aphlox';
-$password = "veronica9";
-//나중에 패스워드 암호화? mb5
-//$sql = "select *from admin where id ='aphlox' and password ='veronica9'";
-$sql = "select *from admin ";
-$res = $conn->query($sql);
+$id = $_POST['id'];
+$pws = $_POST['password'];
 
+//나중에 패스워드 암호화? mb5
+
+$sql = "SELECT *from adminlogin where id = '$id' and password = '$pws'";
+$res = $conn->query($sql);
 if (false === $res) {
     echo mysqli_error();
 }
 $row = mysqli_fetch_array($res);
+
 if($res -> num_rows > 0) {
 $_SESSION['id'] = $id;
 $_SESSION['nickname'] = $row["nickname"];
@@ -23,10 +22,10 @@ if(isset($_SESSION['id']) && isset($_SESSION['nickname'])) {
 echo "<script>location.href='board.php';</script>";
 } else {
 echo "<script>alert('로그인 실패!');</script>";
-//    echo "<script>location.href='login.html';</script>";
+    echo "<script>location.href='login.html';</script>";
 }
 } else {
-    echo "<script>alert($id.'로그인 실패!!');</script>";
-//    echo "<script>location.href='login.html';</script>";
+    echo "<script>alert('로그인 실패!!');</script>";
+    echo "<script>location.href='login.html';</script>";
 }
 ?>
